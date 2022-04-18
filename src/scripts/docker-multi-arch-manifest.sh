26c912a4fccf14a77ae637084f9db70e9a552698
Rafael Ostertag <rafi@guengel.ch>
2022-04-18 08:54:14 +0200

    Extract image tag from pom.xml if not specified

--- src/scripts/docker-multi-arch-manifest.sh
+++ src/scripts/docker-multi-arch-manifest.sh
@@ -1,1 +1,21 @@
+if [ -z "$IMAGE_TAG" ]; then
+  echo "*** No image tag supplied, detect IMAGE_TAG"
+  POM_VERSION=$(mvn -q help:evaluate -DforceStdout -Dexpression='project.version')
+
+  IMAGE_TAG=""
+  if echo "$POM_VERSION" | grep -E '-SNAPSHOT$' >/dev/null; then
+    IMAGE_TAG="latest"
+  else
+    IMAGE_TAG="POM_VERSION"
+  fi
+
+  echo "****"
+  echo "POM_VERSION: ${POM_VERSION}"
+  echo "****"
+fi
+
+echo "****"
+echo "IMAGE_TAG: ${IMAGE_TAG}"
+echo "****"
+
 docker manifest create "${IMAGE_NAME}:${IMAGE_TAG}" --amend "${IMAGE_NAME}:${IMAGE_TAG}-amd64" --amend "${IMAGE_NAME}:${IMAGE_TAG}-arm64"
