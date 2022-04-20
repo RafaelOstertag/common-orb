aa6603a316811b889ec9fc54ce0e943e44a60ca8
Rafael Ostertag <rafi@guengel.ch>
2022-04-20 21:08:13 +0200

    Use non-clashing environment variable name

--- src/scripts/npm-login.sh
+++ src/scripts/npm-login.sh
@@ -1,9 +1,9 @@
-if [ -z "${NPM_TOKEN}" ]
+if [ -z "${NPM_TOKEN_READ}" ]
 then
-  echo "Require \$NPM_TOKEN to be set" >&2
+  echo "Require \$NPM_TOKEN_READ to be set" >&2
   exit 1
 fi
 
 npm config set '@rafaelostertag:registry' 'https://rafaelostertag.jfrog.io/artifactory/api/npm/npm/'
-npm config set '//rafaelostertag.jfrog.io/artifactory/api/npm/npm/:_authToken' "${NPM_TOKEN}"
+npm config set '//rafaelostertag.jfrog.io/artifactory/api/npm/npm/:_authToken' "${NPM_TOKEN_READ}"
 
