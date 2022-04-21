86f63271677a697a08ac53150aa3cd0808c0f745
Rafael Ostertag <rafi@guengel.ch>
2022-04-21 21:45:46 +0200

    Allow for snapshot builds to trigger pipelines

--- src/scripts/maven-trigger-pipeline.sh
+++ src/scripts/maven-trigger-pipeline.sh
@@ -1,8 +1,11 @@
 VERSION=$(mvn -B -q help:evaluate -Dexpression='project.version' -DforceStdout=true)
 
-if echo "${VERSION}" | grep -E -- '-SNAPSHOT$' >/dev/null
+echo "${VERSION}" | grep -E -- '-SNAPSHOT$' >/dev/null
+IS_SNAPSHOT=$?
+
+if [ "${IS_SNAPSHOT}" -eq 0 ] && [ "${ALLOW_SNAPSHOT}" == "false" ]
 then
-  echo "Snapshot version, will not deploy" >&2
+  echo "Snapshot version found, not allowed. If you want snapshot versions to not fail, set ALLOW_SNAPSHOT to 'true'" >&2
   exit 1
 fi
 
